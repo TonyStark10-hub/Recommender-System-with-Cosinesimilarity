9d2b2a264b4854da201cca5c4e64d51cc0da1e70
Tanmay Solanki <tony@MacBook-Pro-3.hitronhub.home>
2020-12-16 13:37:36 -0400

    Cosine similarity

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -5,4 +5,13 @@ Created on Tue Dec 15 14:02:58 2020
 
 @author: tony
 """
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+text=['London Paris London','Paris Paris London']
+cv=CountVectorizer()
+
+count_matrix=cv.fit_transform(text)
+#print(count_matrix.toarray())
+similarity_scores=cosine_similarity(count_matrix)
+print (similarity_scores)
